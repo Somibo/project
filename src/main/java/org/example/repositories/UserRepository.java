90196c5217143d0f56011a42d62dca30d92fe474
Somibo <emiltmil@gmail.com>
2024-03-25 23:59:41 +0300

    1.1.0

--- src/main/java/org/example/repositories/UserRepository.java
+++ src/main/java/org/example/repositories/UserRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-/**
- * @author Neil Alishev
- */
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
